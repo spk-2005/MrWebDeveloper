2de257e992ec1d1cd97e30429a593b2b0d5bc07f
spk-2005 <prasannasimha5002@gmail.com>
2025-08-04 17:58:36 +0530

    test-commit-1

--- pages/index.js
+++ pages/index.js
@@ -669,6 +669,8 @@ export default function Home() {
             })
           }}
         />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4966899358935665"
+     crossorigin="anonymous"></script>
       </Head>
 
       <HomeContent />
