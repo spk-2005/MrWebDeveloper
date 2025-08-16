06b3515814f4ed7b8b4c054a0536379a86b49450
spk-2005 <prasannasimha5002@gmail.com>
2025-08-16 19:51:24 +0530

    redis connected

--- pages/[section]/[item].js
+++ pages/[section]/[item].js
@@ -383,7 +383,7 @@ const OfflineIndicator = React.memo(({ isOnline }) => {
   return (
     <div className="fixed top-16 left-1/2 transform -translate-x-1/2 z-50 bg-red-500 text-white px-4 py-2 rounded-lg shadow-lg flex items-center space-x-2">
       <WifiOff className="w-4 h-4" />
-      <span className="text-sm font-medium">You're offline</span>
+      <span className="text-sm font-medium">You&apos;re offline</span>
     </div>
   );
 });
