e02c17d74abb98f6aa2383093da34d0268bfe25b
spk-2005 <prasannasimha5002@gmail.com>
2025-08-20 14:19:40 +0530

    ads.txt

--- pages/_document.js
+++ pages/_document.js
@@ -38,8 +38,14 @@ export default function Document() {
           data-ad-client="ca-pub-4966899358935665"
           crossOrigin="anonymous"
         ></script>
+        <script async custom-element="amp-auto-ads"
+        src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js">
+</script>
       </Head>
       <body>
+        <amp-auto-ads type="adsense"
+        data-ad-client="ca-pub-4966899358935665">
+</amp-auto-ads>
         <Main />
         <NextScript />
       </body>
